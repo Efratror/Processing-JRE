1c187dc21c51089cfe329627e6f6ebcdebef7e44
M. Avontuur <max.avontuur@hotmail.com>
2022-05-12 09:20:25 +0200

    Add compiler module to JRE build script

--- src/build.bat
+++ src/build.bat
@@ -17,21 +17,21 @@ if exist ..\jre\windows (
   rmdir /s /q ..\jre\windows
 )
 TIMEOUT /T 2 1>>nul
-jlink --no-header-files --no-man-pages --compress=2 --strip-debug --module-path modules;%cd%\openjdk-17.0.2\windows_x64\jmods --add-modules core --output ..\jre\windows || exit /b 1
+jlink --no-header-files --no-man-pages --compress=2 --strip-debug --module-path modules;%cd%\compilerModule\bin;%cd%\openjdk-17.0.2\windows_x64\jmods --add-modules core,compilerModule --output ..\jre\windows || exit /b 1
 
 echo ---Build JRE for linux---
 if exist ..\jre\linux (
   rmdir /s /q ..\jre\linux
 )
 TIMEOUT /T 2 1>>nul
-jlink --no-header-files --no-man-pages --compress=2 --strip-debug --module-path modules;%cd%\openjdk-17.0.2\linux_x64\jmods --add-modules core --output ..\jre\linux || exit /b 1
+jlink --no-header-files --no-man-pages --compress=2 --strip-debug --module-path modules;%cd%\compilerModule\bin;%cd%\openjdk-17.0.2\linux_x64\jmods --add-modules core,compilerModule --output ..\jre\linux || exit /b 1
 
 echo ---Build JRE for macOS---
 if exist ..\jre\macos (
   rmdir /s /q ..\jre\macos
 )
 TIMEOUT /T 2 1>>nul
-jlink --no-header-files --no-man-pages --compress=2 --strip-debug --module-path modules;%cd%\openjdk-17.0.2\macos_x64\Contents\Home\jmods --add-modules core --output ..\jre\macos || exit /b 1
+jlink --no-header-files --no-man-pages --compress=2 --strip-debug --module-path modules;%cd%\compilerModule\bin;%cd%\openjdk-17.0.2\macos_x64\Contents\Home\jmods --add-modules core,compilerModule --output ..\jre\macos || exit /b 1
 echo.
 echo ---JRE's created---
 
