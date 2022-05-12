394c396b7c4f5dbcba87ffda62bee4ace5be8fe4
M. Avontuur <max.avontuur@hotmail.com>
2022-05-12 09:17:44 +0200

    Add compiler module code

---
+++ src/compilerModule/src/module-info.java
@@ -0,0 +1,8 @@
+module compilerModule {
+    requires jdk.compiler;
+    requires java.base;
+    requires java.desktop;
+    requires java.management;
+    requires java.sql;
+    requires java.xml;
+}
