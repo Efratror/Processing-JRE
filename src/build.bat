0377f9e6b2897198218db22484a2a45a0f9c53c4
M. Avontuur <max.avontuur@hotmail.com>
2022-05-16 15:47:24 +0200

    Update build script to use independent sub scrips

--- src/build.bat
+++ src/build.bat
@@ -4,38 +4,30 @@ echo.
 echo ---Building custom JRE for processing---
 echo.
 
+if not exist modules (
+    cd utils
+    call compileModules.bat || exit /b 1
+    cd ../
+)
+
+if not exist compilerModule\bin (
+    cd compilerModule
+    call build.bat || cd ../utils && exit /b 1
+    echo.
+    cd ../
+)
+
 cd utils
-call compileModules.bat || exit /b 1
-call getJDK.bat || exit /b 1
+call createWindowsJRE.bat
+TIMEOUT /T 5 1>>nul
+call createLinuxJRE.bat
+TIMEOUT /T 5 1>>nul
+call createMacOSJRE.bat
 cd ../
-
-echo.
-echo ---Build JRE for windows---
-if exist ..\jre\windows (
-  rmdir /s /q ..\jre\windows
-)
-TIMEOUT /T 2 1>>nul
-jlink --no-header-files --no-man-pages --compress=2 --strip-debug --module-path modules;%cd%\compilerModule\bin;%cd%\openjdk-17.0.2\windows_x64\jmods --add-modules core,compilerModule --output ..\jre\windows || exit /b 1
-
-echo ---Build JRE for linux---
-if exist ..\jre\linux (
-  rmdir /s /q ..\jre\linux
-)
-TIMEOUT /T 2 1>>nul
-jlink --no-header-files --no-man-pages --compress=2 --strip-debug --module-path modules;%cd%\compilerModule\bin;%cd%\openjdk-17.0.2\linux_x64\jmods --add-modules core,compilerModule --output ..\jre\linux || exit /b 1
-
-echo ---Build JRE for macOS---
-if exist ..\jre\macos (
-  rmdir /s /q ..\jre\macos
-)
-TIMEOUT /T 2 1>>nul
-jlink --no-header-files --no-man-pages --compress=2 --strip-debug --module-path modules;%cd%\compilerModule\bin;%cd%\openjdk-17.0.2\macos_x64\Contents\Home\jmods --add-modules core,compilerModule --output ..\jre\macos || exit /b 1
-echo.
 echo ---JRE's created---
 
 TIMEOUT /T 2 1>>nul
 rmdir /s /q modules
-rmdir /s /q openjdk-17.0.2
 
 goto :end
 
