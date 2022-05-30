28688d99a974b3bde6ae67d7c230cdd639554deb
M. Avontuur <max.avontuur@hotmail.com>
2022-05-30 07:39:59 +0200

    Separate compiler from main

--- src/compilerModule/src/com/compiler.java
+++ src/compilerModule/src/com/compiler.java
@@ -5,49 +5,57 @@ import java.util.*;
 
 public class compiler {
     public static void main(String[] arguments) throws IOException {
+
+         compiler comp = new compiler();
+
         for (String s: arguments) {
-            File file = new File(s);
-            
-            JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-            DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
-            StandardJavaFileManager fileManager = compiler.getStandardFileManager(diagnostics, null, null);
-            
-            var classpath = System.getProperty("java.class.path");
-            List<String> options = new ArrayList<String>();
-            options.addAll(Arrays.asList("-classpath", classpath));
-            options = Arrays.asList("-Xdiags:verbose");
-     
-            Iterable<? extends JavaFileObject> compilationUnits =
-                fileManager.getJavaFileObjects(file);
-            compiler.getTask(null, fileManager, diagnostics, options, null, compilationUnits).call();
-     
-            for (Diagnostic<? extends JavaFileObject> diagnostic : diagnostics.getDiagnostics()) {
-                if (diagnostic.getSource() != null) {
-                     String fileUri = diagnostic.getSource().toUri().toString();
-                     String filePath = fileUri.replace("file:///", "");
-     
-                     System.out.format("%s:%s:L%d:C%d:%s%n%s%n",
-                         filePath,
-                         diagnostic.getKind().toString(),                  
-                         diagnostic.getLineNumber(),
-                         diagnostic.getColumnNumber(),
-                         diagnostic.getCode(),
-                         diagnostic.getMessage(null)
-                     );
-                }
-                else {
-                 System.out.format("%s:%s%n%s%n",
-                     diagnostic.getKind().toString(),                  
-                     diagnostic.getCode(),
-                     diagnostic.getMessage(null)
-                 );
-                }
-     
-            }
-                                  
-     
-            fileManager.close();
-         }
+            comp.compile(s);
         }
-      
+    }
+
+    protected void compile(String s) throws IOException{
+        
+       File file = new File(s);
+            
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
+        StandardJavaFileManager fileManager = compiler.getStandardFileManager(diagnostics, null, null);
+        
+        var classpath = System.getProperty("java.class.path");
+        List<String> options = new ArrayList<String>();
+        options.addAll(Arrays.asList("-classpath", classpath));
+        options = Arrays.asList("-Xdiags:verbose");
+    
+        Iterable<? extends JavaFileObject> compilationUnits =
+            fileManager.getJavaFileObjects(file);
+        compiler.getTask(null, fileManager, diagnostics, options, null, compilationUnits).call();
+    
+        for (Diagnostic<? extends JavaFileObject> diagnostic : diagnostics.getDiagnostics()) {
+            if (diagnostic.getSource() != null) {
+                    String fileUri = diagnostic.getSource().toUri().toString();
+                    String filePath = fileUri.replace("file:///", "");
+    
+                    System.out.format("%s:%s:L%d:C%d:%s%n%s%n",
+                        filePath,
+                        diagnostic.getKind().toString(),                  
+                        diagnostic.getLineNumber(),
+                        diagnostic.getColumnNumber(),
+                        diagnostic.getCode(),
+                        diagnostic.getMessage(null)
+                    );
+            }
+            else {
+                System.out.format("%s:%s%n%s%n",
+                    diagnostic.getKind().toString(),                  
+                    diagnostic.getCode(),
+                    diagnostic.getMessage(null)
+                );
+            }
+    
+        }
+                                
+    
+        fileManager.close();
+    }
 }
+      
