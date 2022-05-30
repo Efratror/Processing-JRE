b6c1c3033bcaa9cd5cb1cbe094b98fb9186f9cf2
M. Avontuur <max.avontuur@hotmail.com>
2022-05-30 07:43:19 +0200

    Add argument checking

--- src/compilerModule/src/com/compiler.java
+++ src/compilerModule/src/com/compiler.java
@@ -1,30 +1,129 @@
 package com;
-import javax.tools.*;
+
 import java.io.*;
 import java.util.*;
+import javax.tools.*;
+import java.nio.file.*;
+import java.util.stream.*;
 
 public class compiler {
+
     public static void main(String[] arguments) throws IOException {
+        
+        
+        compiler comp = new compiler();
+        String separator = comp.classPathSeparator();
+        String[] args = comp.checkArguments(arguments, separator);
 
-         compiler comp = new compiler();
-
-        for (String s: arguments) {
-            comp.compile(s);
+        if (!(args[0] == "Missing arguments" || args[0] == "Unknown command" || args[0] == "h")) {
+            comp.compile(args, separator);
         }
+
+        
     }
 
-    protected void compile(String s) throws IOException{
+    protected String classPathSeparator() {
+        String OS = System.getProperty("os.name");
         
-       File file = new File(s);
+        if (OS.startsWith("Windows")){
+            return ";";
+        }
+        else {
+            return":";
+        }
+
+    }
+
+    protected String[] checkArguments(String[] arguments, String separator){
+        String[] args = {"",""};
+        for (int i = 0; i < arguments.length; i += 2) {
             
+            if (arguments.length <= i+1 && arguments[i] != "-h") {
+                args[0] = "Missing arguments";
+                System.out.println(displayHelp());
+                break;
+            }
+
+            switch (arguments[i]) {
+                case "-f":
+                    args[0] = arguments[i+1];
+
+                    break;
+                case "-cp":
+                    if (arguments[i+1].contains(".jar")) {
+                        args[1] = arguments[i+1];
+                        
+                    }
+                    else{
+                        args[1] = listJarFiles(arguments[i+1], separator);
+                    }
+                    break;
+                case "-h":
+                    args[0] = "h";
+                    System.out.println(displayHelp());
+                    break;
+                default:
+                    System.out.println(displayHelp());
+                    args[0] = "Unknown command";
+                    break;
+            }
+        }
+
+        return args;
+    }
+
+    protected String displayHelp(){
+        String message = "";
+        message += "Welkom to the compiler module for the languages server for processing (LS4P)\n";
+        message += "Usage: <options>\n";
+        message += "Where possible options include:\n";
+        message += "-f  <path> file to compile\n";
+        message += "-cp <path> User added class files (imports). This can be a file or a directory\n";
+        message += "-h         This help text\n";
+
+        return message;
+    }
+
+    protected String listJarFiles(String path, String separator) {
+        String jarPaths = "";
+        try (Stream<Path> walk = Files.walk(Paths.get(path))) {
+            // We want to find only regular files
+            List<String> result = walk.filter(Files::isRegularFile)
+                    .map(x -> x.toString()).collect(Collectors.toList());
+            for (String filePath : result) {
+                if (filePath.contains(".jar")){
+                    if (jarPaths == "") {
+                        jarPaths += filePath.replace("\\", "/");
+                    }
+                    else {
+                        jarPaths += separator+filePath.replace("\\", "/");
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return jarPaths;
+    }
+
+    protected boolean compile(String[] args, String seperator) throws IOException{
+        
+        File file = new File(args[0]);
+        
         JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
         DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
         StandardJavaFileManager fileManager = compiler.getStandardFileManager(diagnostics, null, null);
         
-        var classpath = System.getProperty("java.class.path");
+        var javaClasspath = System.getProperty("java.class.path");
         List<String> options = new ArrayList<String>();
-        options.addAll(Arrays.asList("-classpath", classpath));
-        options = Arrays.asList("-Xdiags:verbose");
+        if (javaClasspath != null) {
+            options.addAll(Arrays.asList("-classpath", javaClasspath+seperator+args[1]));
+        }
+        else {
+            options.addAll(Arrays.asList("-classpath", args[1]));
+        }
+        options.addAll(Arrays.asList("-Xdiags:verbose"));
     
         Iterable<? extends JavaFileObject> compilationUnits =
             fileManager.getJavaFileObjects(file);
@@ -52,10 +151,14 @@ public class compiler {
                 );
             }
     
-        }
-                                
+        }                      
     
         fileManager.close();
+
+        if (diagnostics.getDiagnostics().size() > 0 ) {
+            return false;
+        }
+        return true;
     }
-}
       
+}
