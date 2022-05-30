dd352cd9c91c6fb49b51e2c961b2d69b4cfe7153
M. Avontuur <max.avontuur@hotmail.com>
2022-05-30 08:14:21 +0200

    Move files in prep op unit-testing and gradle

--- src/compilerModule/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module compilerModule {
-    requires jdk.compiler;
-    requires java.base;
-    requires java.desktop;
-    requires java.management;
-    requires java.sql;
-    requires java.xml;
-}
